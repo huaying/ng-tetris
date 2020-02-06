081b2a2f2f05e67e97fb99b1fa62be35425a4a2e
huaying <huaying.tsai@verizonmedia.com>
2020-02-06 17:05:34 +0800

    implement main game logic

---
+++ src/app/game.service.ts
@@ -0,0 +1,259 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+import {
+  NUM_ROW,
+  NUM_COLUMN,
+  EMPTY,
+  SHADOW,
+  DIR,
+  DEG,
+  PIECE,
+  LOOP_TIME,
+  GAME_STATUS
+} from "./constants";
+
+function randomItems(items) {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+  grid;
+  gridSubject;
+  status;
+  timer;
+  piece;
+  piecePos;
+  pieceDeg;
+
+  constructor() { 
+    this.status = GAME_STATUS.INIT,
+    this.grid = Array(NUM_ROW).fill(null).map(r => Array(NUM_COLUMN).fill(EMPTY));
+    this.gridSubject = new BehaviorSubject(this.grid);
+    this.timer = null;
+    this.piece = null;
+    this.piecePos = null;
+    this.pieceDeg = null;
+
+    window.addEventListener("keydown", this.controller);
+  }
+
+  getGrid = () => {
+    return this.gridSubject;
+  }
+
+  gameStart() {
+    this.status = GAME_STATUS.PLAYING;
+    window.clearInterval(this.timer);
+    this.timer = setInterval(this.gameLoop, LOOP_TIME);
+    this.placeNewPiece();
+  };
+
+  private controller = e => {
+    const { status, piece, piecePos, pieceDeg } = this;
+    if (status !== GAME_STATUS.PLAYING) return;
+
+    const DIRMap = {
+      37: DIR.LEFT,
+      39: DIR.RIGHT,
+      40: DIR.DOWN
+    };
+
+    // Key Left, Right: move
+    const direction = DIRMap[e.keyCode];
+    if (direction)
+      if (!this.pieceEnded(piece, piecePos, pieceDeg, direction)) {
+        this.updatePiece(piece, piecePos, pieceDeg, direction);
+      }
+
+    // Key Up: rorate
+    if (e.keyCode === 38) {
+      this.rotatePiece(piece, piecePos, pieceDeg);
+    }
+
+    // Key Space: drop
+    if (e.keyCode === 32) {
+      this.dropPiece(piece, piecePos, pieceDeg);
+      this.gameLoop();
+      window.clearInterval(this.timer);
+      this.timer = setInterval(this.gameLoop, LOOP_TIME);
+    }
+  }
+
+  private gameLoop = () => {
+    if (this.pieceEnded(this.piece, this.piecePos, this.pieceDeg, DIR.DOWN)) {
+      this.lineCleanCheck();
+      this.placeNewPiece();
+    } else {
+      this.updatePiece(this.piece, this.piecePos, this.pieceDeg, DIR.DOWN);
+    }
+  };
+
+  private getDropDist = (piece, position, degree) => {
+    const positions = this.getPiecePositions(piece, position, degree);
+    let dist = 0;
+    let next = 0;
+
+    const shouldEnded = ([i, j]) => {
+      return (
+        i + next >= NUM_ROW ||
+        (!positions.map(p => p.toString()).includes([i + next, j].toString()) &&
+          !this.cellEmpty(this.grid[i + next][j]))
+      );
+    };
+
+    while (true) {
+      next = dist + 1;
+      const end = positions.some(shouldEnded);
+      if (!end) dist += 1;
+      else break;
+    }
+    return dist;
+  };
+
+  private dropPiece = (piece, position, degree) => {
+    const dist = this.getDropDist(piece, position, degree);
+    this.updatePiece(piece, position, degree, [dist, 0]);
+  };
+
+  private rotatePiece = (piece, position, degree, cb = () => {}) => {
+    const newGrid = [...this.grid];
+    const nextDeg = (degree + 1) % Object.keys(DEG).length;
+    const positions = this.getPiecePositions(piece, position, degree);
+    const [newPosition, newPositions] = this.getTunedPositions(
+      piece,
+      position,
+      nextDeg
+    );
+
+    if (
+      !newPositions.some(
+        ([i, j]) =>
+          !positions.map(p => p.toString()).includes([i, j].toString()) &&
+          !this.cellEmpty(newGrid[i][j])
+      )
+    ) {
+      positions.forEach(([i, j]) => (newGrid[i][j] = EMPTY));
+      newPositions.forEach(([i, j]) => (newGrid[i][j] = piece));
+    
+      this.piecePos = newPosition;
+      this.pieceDeg = nextDeg;
+      this.grid = newGrid;
+      this.updateShadow(this.piece, this.piecePos, this.pieceDeg);
+    } 
+    cb();
+  };
+
+  private getTunedPositions = (piece, position, degree) => {
+    let positions = null;
+    let prev_pos = [];
+    while (true) {
+      if (prev_pos.toString() === position.toString()) break;
+      prev_pos = [...position];
+      positions = this.getPiecePositions(piece, position, degree);
+      for (let idx = 0; idx < positions.length; idx++) {
+        const [i, j] = positions[idx];
+        if (i < 0) position = [position[0] + 1, position[1]];
+        if (i >= NUM_ROW) position = [position[0] - 1, position[1]];
+        if (j < 0) position = [position[0], position[1] + 1];
+        if (j >= NUM_COLUMN) position = [position[0], position[1] - 1];
+      }
+    }
+    return [position, positions];
+  };
+
+  private pieceEnded = (piece, position, degree, direction) => {
+    const [x, y] = direction;
+    const positions = this.getPiecePositions(piece, position, degree);
+    return positions.some(
+      ([i, j]) =>
+        i + x < 0 ||
+        i + x >= NUM_ROW ||
+        j + y < 0 ||
+        j + y >= NUM_COLUMN ||
+        (!positions
+          .map(p => p.toString())
+          .includes([i + x, j + y].toString()) &&
+          !this.cellEmpty(this.grid[i + x][j + y]))
+    );
+  }
+
+  private cellEmpty = v => v === EMPTY || v === SHADOW;
+
+  private lineCleanCheck = () => {
+    const lines = this.grid.filter(
+      line =>
+        line.some(cell => !this.cellEmpty(cell)) &&
+        !line.every(cell => !this.cellEmpty(cell))
+    );
+
+    const newGrid = Array(NUM_ROW)
+      .fill(null)
+      .map(e => Array(NUM_COLUMN).fill(EMPTY));
+
+    let newGridLineIdx = NUM_ROW - 1;
+    lines.reverse().forEach(line => {
+      newGrid[newGridLineIdx--] = [...line];
+    });
+
+    this.grid = newGrid;
+    // notify component cuz we reassign a new grid to this.grid.
+    this.gridSubject.next(this.grid);
+  };
+  
+  private placeNewPiece = () => {
+    const piece = randomItems(Object.keys(PIECE));
+    const shift = Math.floor((NUM_COLUMN - 1) / 2);
+    const positions = this.getPiecePositions(piece, [0, shift], DEG.ZERO);
+
+    this.updatePiece(piece, [0, shift], DEG.ZERO, [0, 0]);
+  }
+
+  private getPiecePositions = (piece, position, degree) => {
+    return PIECE[piece].shape[degree].map(([i, j]) => [
+      i + position[0],
+      j + position[1]
+    ]);
+  };
+
+  private updatePiece = (piece, position, degree, direction) => {
+    const [x, y] = direction;
+    const newGrid = [...this.grid];
+    const positions = this.getPiecePositions(piece, position, degree);
+
+    positions.forEach(([i, j]) => (newGrid[i][j] = EMPTY));
+    positions.forEach(([i, j]) => (newGrid[i + x][j + y] = piece));
+
+    this.piece = piece;
+    this.pieceDeg = degree;
+    this.piecePos = [position[0] + x, position[1] + y];
+    this.grid = newGrid;
+    this.updateShadow(this.piece, this.piecePos, this.pieceDeg);
+  }
+
+  private updateShadow = (piece, position, degree) => {
+    const newGrid = [...this.grid];
+    const positions = this.getPiecePositions(piece, position, degree);
+
+    newGrid.forEach((row, i) =>
+      row.forEach((unit, j) => {
+        if (unit === SHADOW) newGrid[i][j] = EMPTY;
+      })
+    );
+
+    const dist = this.getDropDist(piece, position, degree);
+    positions.forEach(([i, j]) => {
+      if (
+        !positions.map(p => p.toString()).includes([i + dist, j].toString())
+      ) {
+        newGrid[i + dist][j] = SHADOW;
+      }
+    });
+
+    this.grid = newGrid;
+  };
+
+}
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GameService } from '../game.service';
+
+import {
+  NUM_ROW,
+  NUM_COLUMN,
+  EMPTY,
+  SHADOW,
+  DIR,
+  DEG,
+  PIECE,
+  LOOP_TIME,
+  GAME_STATUS,
+  BLOCK
+} from "../constants";
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  readonly BLOCK = BLOCK;
+  grid;
 
-  constructor() { }
+  constructor(private gameService: GameService) { }
 
   ngOnInit() {
+    this.getGrid();
+    this.gameService.gameStart();
   }
 
+  getGrid() {
+    this.gameService.getGrid().subscribe(grid => this.grid = grid);
+  }
 }
